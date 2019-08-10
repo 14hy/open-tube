456b3c0e070d2a1e18be61690fc516745aec770f
Jeon Ga Bin <hyanghope@naver.com>
2019-08-11 00:54:45 +0900

    #30 api server 프로젝트 구조를 짭니다

--- server/api/common/__init__.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-import config
-
-DATABASE_CONFIG = config.DATABASE
-FLASK_CONFIG = config.FLASK
-
-engine = create_engine(DATABASE_CONFIG.uri, echo=True)
-Base = declarative_base()
