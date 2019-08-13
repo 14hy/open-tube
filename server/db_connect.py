1786f9f97fce00900f0b404cd9e2878275171d9d
Minhyeoky <rhodochrosited@gmail.com>
2019-08-14 01:26:55 +0900

    fix miner config

--- server/db_connect.py
+++ server/db_connect.py
@@ -1,24 +1,8 @@
 import psycopg2
-import configparser
 from sqlalchemy import create_engine
-def config(filename='/server-config/db2.cfg', section='postgresql'):
-    # create a parser
-    parser = configparser.ConfigParser()
-    # read config file
-    parser.read(filename)
- 
-    # get section, default to postgresql
-    db = {}
-    if parser.has_section(section):
-        params = parser.items(section)
-        for param in params:
-            db[param[0]] = param[1]
-    else:
-        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
- 
-    return db
-configs = configparser.ConfigParser()
-configs.read("/server-config/db2.cfg")
-engine = create_engine(configs.get('flask', 'SQLALCHEMY_DATABASE_URI'), encoding='utf-8')
-params = config()
-conn = psycopg2.connect(**params)
+import os
+
+POSTGRES_PASSWORD = os.environ['POSTGRES_PASSWORD']
+
+engine = create_engine(f'postgresql://opentube:{POSTGRES_PASSWORD}@101.101.164.71:32352/opentube', encoding='utf-8')
+conn = psycopg2.connect(host='101.101.167.71', port=32352, user='opentube', database='opentube')
