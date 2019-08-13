d507d2ba7030796daa253d9af8c6ff93c9530fd7
Minhyeoky <rhodochrosited@gmail.com>
2019-08-14 01:28:47 +0900

    Update app.py

--- server/app.py
+++ server/app.py
@@ -2,10 +2,13 @@ from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_restplus import Api
+import os
 
 app = Flask(__name__)
 CORS(app)
-app.config.from_pyfile("/server-config/db.cfg")
+POSTGRES_PASSWORD = os.environ['POSTGRES_PASSWORD']
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://opentube:{POSTGRES_PASSWORD}@101.101.164.71:32352/opentube'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # db 연결
 db = SQLAlchemy(app)
