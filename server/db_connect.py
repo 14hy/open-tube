517585609c4d8787ed6b392d3d77b90dba0392eb
Minhyeoky <rhodochrosited@gmail.com>
2019-08-14 01:41:00 +0900

    Update db_connect.py

--- server/db_connect.py
+++ server/db_connect.py
@@ -5,4 +5,4 @@ import os
 POSTGRES_PASSWORD = os.environ['POSTGRES_PASSWORD']
 
 engine = create_engine(f'postgresql://opentube:{POSTGRES_PASSWORD}@101.101.164.71:32352/opentube', encoding='utf-8')
-conn = psycopg2.connect(host='101.101.167.71', port=32352, user='opentube', database='opentube')
+conn = psycopg2.connect(host='101.101.167.71', port=32352, user='opentube', database='opentube', password=POSTGRES_PASSWORD)
