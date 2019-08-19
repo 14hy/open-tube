fbfa72f3e18b64e4c2afbfab1f39f40a5e9990f5
taeuk-gang <gmdkrl@hanyang.ac.kr>
2019-08-19 18:50:06 +0900

    :up: 영상 박스 코드작성

--- client/src/components/modal-report.js
+++ client/src/components/modal-report.js
@@ -34,13 +34,16 @@ export class ModalReport extends HTMLElement {
 	render() {
 		return html`
         <div class="modal-report"> 
-			<i class="fi-x size-72" @click=${this.clickX}></i>          			
+			<i class="fi-x size-72" @click=${this.clickX}></i>
             <div class="modal-body">				
-				<div class="modal-header">
-					<span class="modal-title">Reports</span>
+				<div class="modal-header">					
+					<h1 class="modal-title"><i class="fi-results size-72"></i> Reports</h1>
 				</div>
 
-				<iframe width="1195" height="600" .src=${this.url} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+				<div class="video-box">
+					<h2 class="video-title"><i class="fi-play-video size-72"></i> Play Video</h2>
+					<iframe width="300" height="250" .src=${this.url} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+				</div>				
 				
 				<div class="modal-footer">© 2019 Open-Tube, Inc.</div>
 			</div>            
