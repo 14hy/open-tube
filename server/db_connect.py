b3c9b528fad9796d374c969f9a21625b509001e9
Minhyeoky <rhodochrosited@gmail.com>
2019-08-20 04:08:38 +0900

    Update db_connect.py

--- server/db_connect.py
+++ server/db_connect.py
@@ -4,6 +4,6 @@ import os
 
 POSTGRES_PASSWORD = os.environ['POSTGRES_PASSWORD']
 
-engine = create_engine(f'postgres://postgres:{POSTGRES_PASSWORD}@210.89.189.25:5432/postgres', encoding='utf-8')
+engine = create_engine(f'postgres://postgres:{POSTGRES_PASSWORD}@210.89.189.25:5432', encoding='utf-8')
 conn = psycopg2.connect(host='210.89.189.25', port=5432, user='postgres', database='postgres',
                         password=POSTGRES_PASSWORD)
