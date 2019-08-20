3f2a0fa098c9d0c3d90e84d3f147ed34c04b14d0
Minhyeoky <rhodochrosited@gmail.com>
2019-08-20 22:53:46 +0900

    Update get_reply_text.py

--- server/controller/get_reply_text.py
+++ server/controller/get_reply_text.py
@@ -3,11 +3,12 @@ from flask_restplus import Resource, Namespace
 from flask import jsonify
 from app import db
 
-api_reply = Namespace('api_reply', description='감성분성 및 키워드 분석')
+api_reply = Namespace('api_reply', description='분석된 댓글 테이블 긁어오기')
 
 
 @api_reply.route("/<vid>")
 class ExtractReply(Resource):
+    
     @api_reply.doc('reply')
     def get(self, vid):
         table = vid.lower()
