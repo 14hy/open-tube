955e7f9e4f8c48b23f91b04fe36c126e43bdf00a
minhyeoklee <rhodochrosited@gmail.com>
2019-08-22 06:38:15 +0900

    :new: thumbnail 기능 및 reply tag image
    + multiprocessing

---
+++ server/models/video.py
@@ -0,0 +1,18 @@
+from app import db
+
+
+class Video(db.Model):
+    __tablename__ = 'video'
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, primary_key=True)
+    vid = db.Column(db.String, nullable=False, unique=True)
+    uid = db.Column(db.String, unique=False, nullable=False)
+    status = db.Column(db.Enum('wait', 'processing', 'complete', name='video_type'),
+                       nullable=False, default=False)
+    reply_gif = db.Column(db.JSON(none_as_null=True), nullable=True, unique=False)
+    thumbnails_path = db.Column(db.JSON(none_as_null=True), unique=False, nullable=True)
+    
+
+if __name__ == "__main__":
+    db.create_all()
+    # db.drop_all()
---
+++ server/src/utils.py
@@ -0,0 +1,8 @@
+
+# OPEN CV HELPER VALUES
+CV_CAP_PROP_POS_MSEC = 0  # Current position of the video file in milliseconds or video capture timestamp.
+CV_CAP_PROP_POS_FRAMES = 1  # 0-based index of the frame to be decoded/captured next.
+CV_CAP_PROP_FRAME_WIDTH = 3  # Width of the frames in the video stream.
+CV_CAP_PROP_FRAME_HEIGHT = 4  # Height of the frames in the video stream.
+CV_CAP_PROP_FPS = 5  # Frame rate.
+CV_CAP_PROP_FRAME_COUNT = 7  # Number of frames in the video file.
