1189680f0ab2eb486ad55ea3cc954aacd5e0166c
Minhyeoky <rhodochrosited@gmail.com>
2019-08-22 21:44:08 +0900

    typo fix

--- server/run.py
+++ server/run.py
@@ -1,5 +1,5 @@
 from app import app
 
 if __name__ == "__main__":
-    context = ('/mnt/master/https_keys/certificate.crt', '/mnt/master/root/https_keys/private.key')
+    context = ('/mnt/master/https_keys/certificate.crt', '/mnt/master/https_keys/private.key')
     app.run(host="0.0.0.0", port=8888, debug=True, ssl_context=context)
