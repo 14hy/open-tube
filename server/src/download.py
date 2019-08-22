6aa92de910ef580636640e73b410cf89c15b03b8
Minhyeoky <rhodochrosited@gmail.com>
2019-08-23 01:45:55 +0900

    Update download.py

--- server/src/download.py
+++ server/src/download.py
@@ -390,6 +390,85 @@ def _greedy_nms2(predictions, iou_threshold=0.45, coords='minmax'):
         boxes_left = boxes_left[similarities <= iou_threshold] # ...so that we can remove the ones that overlap too much with the maximum box	
     return np.array(maxima)
 
+
+def convert_coordinates(tensor, start_index, conversion='minmax2centroids'):
+    '''
+    Convert coordinates for axis-aligned 2D boxes between two coordinate formats.
+    Creates a copy of `tensor`, i.e. does not operate in place. Currently there are
+    two supported coordinate formats that can be converted from and to each other:
+        1) (xmin, xmax, ymin, ymax) - the 'minmax' format
+        2) (cx, cy, w, h) - the 'centroids' format
+    Note that converting from one of the supported formats to another and back is
+    an identity operation up to possible rounding errors for integer tensors.
+    Arguments:
+        tensor (array): A Numpy nD array containing the four consecutive coordinates
+            to be converted somewhere in the last axis.
+        start_index (int): The index of the first coordinate in the last axis of `tensor`.
+        conversion (str, optional): The conversion direction. Can be 'minmax2centroids'
+            or 'centroids2minmax'. Defaults to 'minmax2centroids'.
+    Returns:
+        A Numpy nD array, a copy of the input tensor with the converted coordinates
+        in place of the original coordinates and the unaltered elements of the original
+        tensor elsewhere.
+    '''
+    ind = start_index
+    tensor1 = np.copy(tensor).astype(np.float)
+    if conversion == 'minmax2centroids':
+        tensor1[..., ind] = (tensor[..., ind] + tensor[..., ind+1]) / 2.0 # Set cx
+        tensor1[..., ind+1] = (tensor[..., ind+2] + tensor[..., ind+3]) / 2.0 # Set cy
+        tensor1[..., ind+2] = tensor[..., ind+1] - tensor[..., ind] # Set w
+        tensor1[..., ind+3] = tensor[..., ind+3] - tensor[..., ind+2] # Set h
+    elif conversion == 'centroids2minmax':
+        tensor1[..., ind] = tensor[..., ind] - tensor[..., ind+2] / 2.0 # Set xmin
+        tensor1[..., ind+1] = tensor[..., ind] + tensor[..., ind+2] / 2.0 # Set xmax
+        tensor1[..., ind+2] = tensor[..., ind+1] - tensor[..., ind+3] / 2.0 # Set ymin
+        tensor1[..., ind+3] = tensor[..., ind+1] + tensor[..., ind+3] / 2.0 # Set ymax
+    else:
+        raise ValueError("Unexpected conversion value. Supported values are 'minmax2centroids' and 'centroids2minmax'.")
+
+    return tensor1
+
+def iou(boxes1, boxes2, coords='centroids'):
+    '''
+    Compute the intersection-over-union similarity (also known as Jaccard similarity)
+    of two axis-aligned 2D rectangular boxes or of multiple axis-aligned 2D rectangular
+    boxes contained in two arrays with broadcast-compatible shapes.
+    Three common use cases would be to compute the similarities for 1 vs. 1, 1 vs. `n`,
+    or `n` vs. `n` boxes. The two arguments are symmetric.
+    Arguments:
+        boxes1 (array): Either a 1D Numpy array of shape `(4, )` containing the coordinates for one box in the
+            format specified by `coords` or a 2D Numpy array of shape `(n, 4)` containing the coordinates for `n` boxes.
+            Shape must be broadcast-compatible to `boxes2`.
+        boxes2 (array): Either a 1D Numpy array of shape `(4, )` containing the coordinates for one box in the
+            format specified by `coords` or a 2D Numpy array of shape `(n, 4)` containing the coordinates for `n` boxes.
+            Shape must be broadcast-compatible to `boxes1`.
+        coords (str, optional): The coordinate format in the input arrays. Can be either 'centroids' for the format
+            `(cx, cy, w, h)` or 'minmax' for the format `(xmin, xmax, ymin, ymax)`. Defaults to 'centroids'.
+    Returns:
+        A 1D Numpy array of dtype float containing values in [0,1], the Jaccard similarity of the boxes in `boxes1` and `boxes2`.
+        0 means there is no overlap between two given boxes, 1 means their coordinates are identical.
+    '''
+
+    if len(boxes1.shape) > 2: raise ValueError("boxes1 must have rank either 1 or 2, but has rank {}.".format(len(boxes1.shape)))
+    if len(boxes2.shape) > 2: raise ValueError("boxes2 must have rank either 1 or 2, but has rank {}.".format(len(boxes2.shape)))
+
+    if len(boxes1.shape) == 1: boxes1 = np.expand_dims(boxes1, axis=0)
+    if len(boxes2.shape) == 1: boxes2 = np.expand_dims(boxes2, axis=0)
+
+    if not (boxes1.shape[1] == boxes2.shape[1] == 4): raise ValueError("It must be boxes1.shape[1] == boxes2.shape[1] == 4, but it is boxes1.shape[1] == {}, boxes2.shape[1] == {}.".format(boxes1.shape[1], boxes2.shape[1]))
+
+    if coords == 'centroids':
+        # TODO: Implement a version that uses fewer computation steps (that doesn't need conversion)
+        boxes1 = convert_coordinates(boxes1, start_index=0, conversion='centroids2minmax')
+        boxes2 = convert_coordinates(boxes2, start_index=0, conversion='centroids2minmax')
+    elif coords != 'minmax':
+        raise ValueError("Unexpected value for `coords`. Supported values are 'minmax' and 'centroids'.")
+
+    intersection = np.maximum(0, np.minimum(boxes1[:,1], boxes2[:,1]) - np.maximum(boxes1[:,0], boxes2[:,0])) * np.maximum(0, np.minimum(boxes1[:,3], boxes2[:,3]) - np.maximum(boxes1[:,2], boxes2[:,2]))
+    union = (boxes1[:,1] - boxes1[:,0]) * (boxes1[:,3] - boxes1[:,2]) + (boxes2[:,1] - boxes2[:,0]) * (boxes2[:,3] - boxes2[:,2]) - intersection
+
+    return intersection / union
+
 if __name__ == '__main__':
     # download_url('https://www.youtube.com/watch?v=Vp3fWnf1DoM')
     pass
