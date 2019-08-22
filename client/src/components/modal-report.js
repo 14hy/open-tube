339e7de2d90add86991163b4cb7a1693e96f5475
taeuk-gang <gmdkrl@hanyang.ac.kr>
2019-08-22 09:33:16 +0900

    :new: 레포트 - 얼굴 인식 (2)

--- client/src/components/modal-report.js
+++ client/src/components/modal-report.js
@@ -11,13 +11,14 @@ export class ModalReport extends HTMLElement {
 
 		this.url = ``
 		this.vid = ``
-		this.title = ``
+		this.titles = ``
 		this.desc = ``
 		this.date = ``
 		this.viewCount = ``
 		this.likeCount = ``
 
 		this.done = false
+		this.stateFaceImg = true
 	}
 
 	connectedCallback() {
@@ -30,10 +31,10 @@ export class ModalReport extends HTMLElement {
 			window.player = new YT.Player(`player`, {
 				height: `250`,
 				width: `300`,
-				videoId: `M7lc1UVf-VE`,
+				videoId: `M7lc1UVf-V`,
 				events: {
-					'onReady': this.onPlayerReady,
-					'onStateChange': this.onPlayerStateChange,
+					/* 'onReady': this.onPlayerReady, */
+					/* 'onStateChange': this.onPlayerStateChange.bind(this), */
 				},
 				playerVars: {
 					fs: 0,
@@ -48,12 +49,12 @@ export class ModalReport extends HTMLElement {
 	}
 
 	onPlayerReady(event) {
-		event.target.stopVideo()
+		event.target.playVideo()
 	}
 
 	onPlayerStateChange(event) {
 		if (event.data == YT.PlayerState.PLAYING && !this.done) {
-			setTimeout(this.stopVideo, 6000)
+			setTimeout(this.stopVideo, 1000)
 			this.done = true
 		}
 	}
@@ -66,7 +67,8 @@ export class ModalReport extends HTMLElement {
 		this.url = url
 		this.style.transform = `scale(1)`
 		this.getYoutubeData(vid)
-		window.player.cueVideoById(vid)
+		window.player.loadVideoById(vid)
+		window.setTimeout(() => window.player.pauseVideo(), 2000)
 		render(this.render(), this)
 	}
 
@@ -84,11 +86,25 @@ export class ModalReport extends HTMLElement {
 		}
 	}
 
+	get clickFullScreen() {
+		const root = this
+		return {
+			handleEvent() {
+				if (document.fullscreenElement) {
+					document.exitFullscreen()
+				} else {
+					root.querySelector(`.player-wrap`).requestFullscreen()
+				}				
+			},
+			capture: true,
+		}
+	}
+
 	async getYoutubeData(vid) {		
 		const text = await xhrCorsServer(`https://www.youtube.com/watch?v=${vid}`)
 
 		this.vid = vid
-		this.title = text.match(/videoPrimaryInfoRenderer":{"title":{"runs":\[{"text":"(.*?)"}\]}/)[1]
+		this.titles = text.match(/videoPrimaryInfoRenderer":{"title":{"runs":\[{"text":"(.*?)"}\]}/)[1]
 		this.viewCount = text.match(/"viewCountText":{"simpleText":"조회수 (.*?)"}/)[1]
 		this.date = text.match(/"dateText":{"simpleText":"게시일: (.*?)"}/)[1]
 		this.desc = text.match(/"description":{"runs":\[{"text":"(.*?)"}/)[1]
@@ -105,11 +121,13 @@ export class ModalReport extends HTMLElement {
 			<!-- <iframe width="300" height="250" .src=${this.url} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
 			<div class="player-wrap">
 				<div id="player"></div>
+				<i class="fi-arrows-out size-72 player-full-screen" @click=${this.clickFullScreen}></i>
+				<span class="sensor-box"></span>
 			</div>			
 			<h2 class="video-desc">Information</h2>
 			<div class="video-desc">
 				<span class="desc-title">Title:</span>
-				<span class="desc-content title">${this.title}</span>
+				<span class="desc-content title">${this.titles}</span>
 			</div>
 			<div class="video-desc">
 				<span class="desc-title">Date:</span>
@@ -139,13 +157,7 @@ export class ModalReport extends HTMLElement {
 
 		const res = await postXhr(`http://101.101.167.71:32666/download/`, formData)
 
-		const status = JSON.parse(res)[`status`]				
-
-		// data.forEach(img => {
-		// 	img.forEach(time => {				
-		// 		console.log(Object.entries(time)[0])				
-		// 	})
-		// })
+		const status = JSON.parse(res)[`status`]
 
 		if (status === `wait`) {
 			render(html `${i18next.t(`MODAL_REPORT_FACE_STATUS_WAIT`)}`, this.querySelector(`.face-content`))	
@@ -153,16 +165,140 @@ export class ModalReport extends HTMLElement {
 			render(html `${i18next.t(`MODAL_REPORT_FACE_STATUS_PROCESSING`)}`, this.querySelector(`.face-content`))	
 		} else if (status === `complete`) {
 			const data = Object.values(JSON.parse(JSON.parse(res)[`time_line`]))
-			const imgCount = data.length
+			const imgCount = data.length			
 
 			render(html `
 				${imgCount}개의 이미지가 검색됨
 			`, this.querySelector(`.face-img-count`))
 			
 			render(html `
-				${data.map((img, index) => html`<img class="face-img" style="border: 5px solid ${randomcolor({luminosity: `dark`})}" src="https://open-tube.kro.kr/img-face/${uid}/${this.vid}/${index}.jpg"/>`)}
-			`, this.querySelector(`.face-content`))	
+				${data.map((img, index) => html`
+				<img
+					@click=${this.mouseclickFaceImg(index, data)}
+					@mouseenter=${this.mouseenterFaceImg(index, data)}
+					@mouseleave=${this.mouseleaveFaceImg}
+					class="face-img" 
+					style="border: 5px solid ${randomcolor({luminosity: `dark`})}" 
+					src="https://open-tube.kro.kr/img-face/${uid}/${this.vid}/${index}.jpg"/>
+				`)}
+			`, this.querySelector(`.face-content`))
 		}
+	}	
+
+	mouseclickFaceImg(index, data) {
+		const root = this
+		return {
+			handleEvent(event) {
+				const infoBox = root.querySelector(`.face-info-box`)
+				const domRect = event.target.getBoundingClientRect()
+				event.target.classList.toggle(`active`)
+				if (event.target.classList.contains(`active`)) {
+					root.stateFaceImg = false
+					infoBox.style.pointerEvents = `all`
+					infoBox.style.opacity = `0.9`					
+					infoBox.style.top = `${domRect.top}px`
+					infoBox.style.left = `${domRect.left + domRect.width}px`
+
+					root.renderFaceInfo(data, index)
+				} else {
+					root.stateFaceImg = true
+					infoBox.style.pointerEvents = `none`
+				}
+			},
+			capture: true,
+		}
+	}
+
+	mouseenterFaceImg(index, data) {
+		const root = this
+		return {
+			handleEvent(event) {				
+				if (root.stateFaceImg) {
+					const domRect = event.target.getBoundingClientRect()
+					const span = root.querySelector(`.face-info-box`)
+					span.style.opacity = `0.8`
+					span.style.top = `${domRect.top}px`
+					span.style.left = `${domRect.left + domRect.width}px`
+					
+					root.renderFaceInfo(data, index)
+				}
+			},
+			capture: true,
+		}
+	}
+
+	renderFaceInfo(data, index) {
+		data.forEach((img, _index) => {
+			if (index === _index) {
+				render(html`
+				${img.map(time => {
+		const obj = Object.entries(time)[0]
+		const seconds = obj[0]
+		const x = parseFloat(obj[1][0]).toFixed(2)
+		const y = parseFloat(obj[1][1]).toFixed(2)
+		const width = parseFloat(obj[1][2]).toFixed(2)
+		const height = parseFloat(obj[1][3]).toFixed(2)
+
+		return this.timeDesc({
+			seconds,
+			x,
+			y,
+			width,
+			height,
+		})
+	})}
+				`, this.querySelector(`.time-desc-wrap`))				
+			}			
+		})
+	}
+
+	get mouseleaveFaceImg() {
+		const root = this
+		return {
+			handleEvent() {
+				if (root.stateFaceImg) {
+					const span = root.querySelector(`.face-info-box`)
+					span.style.opacity = `0`
+				}
+			},
+			capture: true,
+		}
+	}
+
+	get clickFaceImgX() {
+		const root = this
+		return {
+			handleEvent() {
+				const infoBox = root.querySelector(`.face-info-box`)
+				root.stateFaceImg = true
+				infoBox.style.pointerEvents = `none`
+			},
+			capture: true,
+		}
+	}
+
+	get clickSeconds() {
+		return {
+			handleEvent(event) {
+				const time = event.target.textContent
+
+				window.player.seekTo(time)
+				window.player.pauseVideo()
+			},
+			capture: true,
+		}
+	}
+
+	timeDesc(obj = null) {
+		return html`
+		<div class="time-desc">
+			<div class="desc-seconds"><h5>Seconds</h5><span class="second-detail" @click=${this.clickSeconds}>${obj.seconds}</span></div>
+			<div class="desc-x"><h5>X: </h5>${obj.x}</div>
+			<div class="desc-y"><h5>Y: </h5>${obj.y}</div>
+			<div class="desc-width"><h5>Width: </h5>${obj.width}</div>
+			<div class="desc-height"><h5>Height: </h5>${obj.height}</div>
+		</div>
+		`
 	}
 
 	get faceAnalysisBox() {		
@@ -173,6 +309,11 @@ export class ModalReport extends HTMLElement {
 			<div class="face-content"></div>
 			<h3 class="title"><i class="fi-photo size-72"></i> Images by Time <span class="face-img-count"></span></h3>
 			<div class="face-content"></div>
+			<span class="face-info-box">
+				<i class="fi-x size-72" @click=${this.clickFaceImgX}></i>
+				<h4 class="img-title">Image Time · Position</h4>
+				<div class="time-desc-wrap"></div>
+			</span>			
 		</span>
 		`
 	}
