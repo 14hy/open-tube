dad667c61d4299a6598100a4c251f885cca88d98
taeuk-gang <gmdkrl@hanyang.ac.kr>
2019-08-23 16:06:47 +0900

    :up: 레포트 - 레포트별 상태 분류 (2)

--- client/src/components/filter-list.js
+++ client/src/components/filter-list.js
@@ -30,6 +30,54 @@ export class FilterList extends HTMLElement {
 		}
 	}
 
+	get clickWait() {
+		const root = this
+		return {
+			handleEvent(event) { 
+				const check = event.currentTarget.checked
+
+				root.showOrHide(check, `대기 중`)
+			},
+			capture: false,
+		}
+	}
+
+	get clickProcessing() {
+		const root = this
+		return {
+			handleEvent(event) { 
+				const check = event.currentTarget.checked
+
+				root.showOrHide(check, `분석 중`)
+			},
+			capture: false,
+		}
+	}
+
+	get clickComplete() {
+		const root = this
+		return {
+			handleEvent(event) { 
+				const check = event.currentTarget.checked
+
+				root.showOrHide(check, `분석 완료`)
+			},
+			capture: false,
+		}
+	}
+
+	showOrHide(check, text) {
+		document.querySelectorAll(`report-list > ul > li`).forEach(li => {
+			if (li.querySelector(`.report-status`).textContent === text) {
+				if (check) {
+					li.style.display = `list-item`
+				} else {
+					li.style.display = `none`
+				}
+			}
+		})
+	}
+
 	render() {
 		return html`
         <h2 class="category-title">Reports</h2>
@@ -40,9 +88,9 @@ export class FilterList extends HTMLElement {
             Report Step
         </h3>
         <ul class="filter-ul">
-            <li><input type="checkbox" id="li1" checked/><label for="li1">대기 중</label></li>
-			<li><input type="checkbox" id="li2" checked/><label for="li2">분석 중</label></li>
-			<li><input type="checkbox" id="li3" checked/><label for="li3">분석 완료</label></li>
+            <li><input type="checkbox" id="li1" checked @click=${this.clickWait}/><label for="li1">대기 중</label></li>
+			<li><input type="checkbox" id="li2" checked @click=${this.clickProcessing}/><label for="li2">분석 중</label></li>
+			<li><input type="checkbox" id="li3" checked @click=${this.clickComplete}/><label for="li3">분석 완료</label></li>
         </ul>
         <button class="btn-request-report" @click=${this.clickRequestReport()}>${i18next.t(`BTN_REQUEST_REPORT`)}</button>
         `
